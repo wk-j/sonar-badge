472260e21df88d65d42141eb38db784325c64e12
wk-j <somnuk.wk@gmail.com>
2018-08-07 16:49:45 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Sonar Badge
+
+Installation
+
+```bash
+dotnet tool install -g wk.SonarBadge
+```
+
+Usage
+
+```bash
+wk-sonar-badge http://localhost:9000 project-name
+```
---
+++ src/SonarBadge/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace SonarBadge {
+    class Program {
+        static void Main(string[] args) {
+            var host = args[0];
+            var project = args[1];
+
+            // http://localhost:9000/api/project_badges/measure?project=easy-validator&metric=alert_status
+            // http://localhost:9000/dashboard?id=easy-capture
+
+            var metrics = new[] {
+                "alert_status",
+                "reliability_rating",
+                "security_rating",
+                "bugs",
+                "code_smells",
+                "sqale_index",
+                "vulnerabilities",
+                "ncloc",
+                "duplicated_lines_density"
+            };
+
+            var markdowns = metrics.Select(metric => {
+                var md = $@"[![]({host}/api/project_badges/measure?project={project}&metric={metric})]({host}/dashboard?id={project})";
+                return md;
+            });
+
+            foreach (var md in markdowns) {
+                Console.WriteLine(md);
+            }
+        }
+    }
+}
